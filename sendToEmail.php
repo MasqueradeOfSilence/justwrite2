76904c45cb48785e5b693d09e29b64697b703cb9
Alex <alexcn711@gmail.com>
2022-07-08 16:35:15 -0600

    schedule form is working

--- sendToEmail.php
+++ sendToEmail.php
@@ -1,30 +1,6 @@
 <?php
-  if($_POST["message"])
-  {
-    // TODO: replace this with captcha
-    if (true)
-    {
-        /*
-        - requesterName
-        - attorneyName
-        - firmName
-        - address1
-        - address2
-        - phoneNumber
-        - email
-        - caseName
-        - caseNumber
-        - date
-        - time
-        - locationInformation -- NOTE: This is a radio with multiple choices that I didn't put in yet. I will need to look more into this. 
-        - proceedingDeponents
-        - estimatedLength
-        - comments
-        */
-      // depos@justwritecr.com
-      mail("softwaredev@alexanderneville.com",
+    mail("softwaredev@alexanderneville.com",
         "NEW DEPOSITION SCHEDULING REQUEST",
-        // Message
         "Requester Name: " . $_POST["requesterName"] . "\n"
         . "Attorney Name: " . $_POST["attorneyName"] . "\n"
         . "Firm Name: " . $_POST["firmName"] . "\n"
@@ -34,36 +10,43 @@
         . "Email: " . $_POST["email"] . "\n"
         . "Case Name: " . $_POST["caseName"] . "\n"
         . "Case Number: " . $_POST["caseNumber"] . "\n"
-        // Date TBD
-        // Time TBD
-        // Location Info TBD
+        . "Date: " . $_POST["date"] . "\n"
+        . "Time: " . $_POST["time"] . "\n"
+        . "Location: " . $_POST["locationInformation"] . "\n"
+        . "Additional Location Details, if any: " . $_POST["locationSpecific"] . "\n"
         . "Proceeding Deponents: " . $_POST["proceedingDeponents"] . "\n"
         . "Estimated Length: " . $_POST["estimatedLength"] . "\n"
-        . "Comments: " . $_POST["comments"] . "\n"
-        , 
+        . "Comments: " . $_POST["comments"] . "\n",
         "From: " . $_POST["requesterName"] . ", " . $_POST["email"]);
 
-      echo '<script type="text/JavaScript">
+    mail($_POST["email"], 
+        "Deposition Scheduled",
+        "Your deposition schedule request has been created online. We are currently processing your request. Details: \n\n" . 
+        "Requester Name: " . $_POST["requesterName"] . "\n"
+        . "Attorney Name: " . $_POST["attorneyName"] . "\n"
+        . "Firm Name: " . $_POST["firmName"] . "\n"
+        . "Address Line 1: " . $_POST["address1"] . "\n"
+        . "Address Line 2: " . $_POST["address2"] . "\n"
+        . "Phone Number: " . $_POST["phoneNumber"] . "\n"
+        . "Email: " . $_POST["email"] . "\n"
+        . "Case Name: " . $_POST["caseName"] . "\n"
+        . "Case Number: " . $_POST["caseNumber"] . "\n"
+        . "Date: " . $_POST["date"] . "\n"
+        . "Time: " . $_POST["time"] . "\n"
+        . "Location: " . $_POST["locationInformation"] . "\n"
+        . "Additional Location Details, if any: " . $_POST["locationSpecific"] . "\n"
+        . "Proceeding Deponents: " . $_POST["proceedingDeponents"] . "\n"
+        . "Estimated Length: " . $_POST["estimatedLength"] . "\n"
+        . "Comments: " . $_POST["comments"] . "\n",
+        "From Just Write Court Reporters, depos@justwritecr.com.");
+        
+    echo '<script type="text/JavaScript">
         window.alert("Scheduling request sent successfully.");
         </script>';
 
-      echo '<script type="text/JavaScript">
+    echo '<script type="text/JavaScript">
         window.location = document.referrer;
         </script>';
 
       exit;
-    }
-    else
-    {
-      echo '<script type="text/JavaScript">
-        window.alert("Scheduling request failed.");
-        </script>';
-
-      echo '<script type="text/JavaScript">
-        window.location = document.referrer;
-        </script>';
-
-      exit;
-    }
-  }
 ?>
